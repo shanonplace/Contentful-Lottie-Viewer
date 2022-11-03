dc5df3ff2b810192e759f96160180855db094691
Shanon Place <shanonplace@gmail.com>
2022-11-03 10:51:15 -0600

    Added in the little edit button

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSDK } from "@contentful/react-apps-toolkit";
 import { JsonEditor } from "@contentful/field-editor-json";
 import Lottie from "react-lottie-player";
-import { Flex } from "@contentful/f36-components";
+import { Button } from "@contentful/f36-components";
 
 const Field = () => {
   const [lottieJson, setLottieJson] = useState("");
@@ -26,6 +26,7 @@ const Field = () => {
 
   return (
     <>
+      <Button variant="primary">Edit Lottie</Button>
       <Lottie
         loop
         animationData={lottieJson}
