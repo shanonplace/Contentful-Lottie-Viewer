b2385292b5bd5203a616a7ce53eebeb4d4b20f8d
Shanon Place <shanonplace@gmail.com>
2022-11-03 10:26:16 -0600

    Added a new small example and removed a specific reference to a field name as that wouldn't be portable to any json field

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -20,20 +20,11 @@ const Field = () => {
   useEffect(() => {
     sdk.window.startAutoResizer();
 
-    sdk.entry.fields.animatedImage.onValueChanged((value) => {
+    sdk.field.onValueChanged((value) => {
       setLottieJson(value ? value : "");
     });
-  }, [sdk.entry.fields.animatedImage, sdk.window]);
+  }, [sdk.field, sdk.window]);
 
-  //const lottieJson = sdk.field.getValue();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
   return (
     <>
       <Lottie
